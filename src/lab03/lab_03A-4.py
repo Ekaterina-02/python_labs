a55d346807346f57b1f4b94c56667832e613c90a
Ekaterina-02 <k89806883008@gmail.com>
2025-10-22 15:33:41 +0300

    kjhgfgh

--- src/lab03/lab_03A-4.py
+++
@@ -1,6 +0,0 @@
-def top_n(freq: dict[str, int], n: int = 5) -> list[tuple[str, int]]:
-    spisoc=list(freq.items()) 
-    sorted_spisoc=sorted(spisoc, key=lambda x: x[1], reverse=True)
-    return sorted_spisoc[:n]
-print(top_n({"bb": 2, "aa": 2, "cc": 1}))
-print(top_n({"bb": 2, "aa": 2, "cc": 1}, n=2))
